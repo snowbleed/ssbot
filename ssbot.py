1ab83944ad28765edf54c0dac7653dbd1c9dbd14
snowbleed <45135758+snowbleed@users.noreply.github.com>
2019-03-27 21:46:58 +0100

    Update ssbot.py

--- ssbot.py
+++ ssbot.py
@@ -1,1 +1,122 @@
 import discord
+from discord.ext import commands
+import asyncio
+import time
+import datetime
+import json
+import aiohttp
+import io 
+
+TOKEN = 'NTYwNTY1MjgyNzc1NzYwOTA2.D31yuA.rDyIhClS0pldqk_TmsF_5tAjdZU'
+
+client = commands.Bot(command_prefix = '>')
+client.authorizedusers = ["147999751441219584"]
+#                          1479 = snowbleed      
+client.remove_command("help")
+extensions = ['CommandErrorHandler']
+             
+@client.event
+async def on_ready():
+    print("Bot is online")
+    
+@client.command()
+async def cmds():
+    embed = discord.Embed(
+    title = 'Made by `snowbleed#7824`',
+    description = '**List of commands:**',
+    timestamp = datetime.datetime.utcnow(),
+    colour = discord.Colour.blue()
+    )
+    embed.set_footer(text='usms bot')
+    embed.add_field(name=";cmds", value="Display a list of commands.", inline = False)
+    embed.add_field(name=";ping", value="Check if the bot is responding quickly and correctly.", inline = False)
+   # embed.add_field(name=";request", value="Request protection if you are a protectee. Format: `;request <place> <message>` where <place> is either lv, dc, ch or jag, e.g.: ;request LV Big House 1", inline = False)
+    embed.add_field(name=";protectee", value="See if a person is a protectee and if so why. Format: `;protectee <name>` where <name> is their ROBLOX username", inline = False)
+    await client.say(embed=embed)
+    
+"""
+@client.command(pass_context=True)
+async def request(ctx, arg1, *, args):
+	with open("text.json", "r") as read_file:
+		usernames = json.load(read_file)
+	memberid = ctx.message.author.id
+	if arg1.lower() == 'lv':
+		city='Las Vegas *https://www.roblox.com/games/163865146/LV*'
+	elif arg1.lower() == 'dc':
+		city='Washington D.C. *https://www.roblox.com/games/1213026131/DC*'
+	elif arg1.lower() == 'ch':
+		city='Federal Courthouse *https://www.roblox.com/games/489987188/USA-United-States-Federal-Courthouse*'
+	elif arg1.lower() == 'jag':
+		city='JAG Courthouse *https://www.roblox.com/games/2776040253/JAG-Court-House*'
+	else:
+		await client.say("Place needs to be either `LV`, `DC`, `CH` (Federal Courthouse), or `JAG` (Military Courthouse)")
+		return
+	if args == '':
+		args = 'N/A'
+	server = client.get_server('511646082200240131')
+	role = discord.utils.get(server.roles, name='USMS')
+	for username, metadata in usernames.items():
+		if memberid == metadata['userid']:
+			honorem = metadata['honorem']
+			await client.send_message(client.get_channel('549431202772418567'),f"**PROTECTION ANNOUNCEMENT:**\n{honorem} {username} has requested *immediate* protection at {city}\n\nMessage from protectee: {args}\n\n{role.mention}")
+			break
+	else:
+		await client.say("You are not a protectee.")
+"""
+
+
+@client.command(pass_context=True)
+async def protectee(ctx, name):
+	with open("text.json", "r") as read_file:
+		usernames = json.load(read_file)
+	memberid = ctx.message.author.id
+	for x in usernames:
+		if x.lower() == name.lower():
+			honorem = usernames[x]['honorem']
+			await client.say(f"{name.capitalize()} is a protectee, honorem: **{honorem}**.")
+			break
+	else:
+		await client.say(f"{name.capitalize()} is not a protectee.")
+	
+@client.command(pass_context=True)
+async def justtest(ctx):
+	async with aiohttp.ClientSession() as client_session:
+		async with client_session.get("https://i.imgur.com/j2P5uXE.jpg") as response:
+			my_file_like_object = io.BytesIO(await response.read())
+
+	await client.send_file(destination=ctx.message.channel, fp=my_file_like_object, filename="stopreadingthis.png")
+    
+@client.command()
+async def ping():
+    await client.say('Pong!')        
+	
+@client.command(pass_context=True)
+async def logout(ctx):
+    if ctx.message.author.id == "147999751441219584":
+        await client.logout()
+    else:
+        return
+"""EMBED EXAMPLE     
+@client.command()
+async def displayembed():
+    embed = discord.Embed(
+        title = 'Title',
+        description = 'This is a [description](https://youtube.com) link',
+        colour = discord.Colour.blue()
+    )
+    embed.set_footer(text='This is a footer')
+    embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/518210617744818206/518463077541216258/kava.jpg')
+    embed.set_author(name='Snowbleed')
+    icon_url='https://cdn.discordapp.com/attachments/518210617744818206/518463077541216258/kava.jpg'
+    embed.add_field(name='Snowbleed', value='Field Value', inline = False)
+    embed.add_field(name='Snowbleed', value='Field Value', inline = True)
+    embed.add_field(name='Snowbleed', value='Field Value', inline = True)
+    await client.say(embed=embed)"""
+
+if __name__ == '__main__':
+	for extension in extensions:
+		try:
+			client.load_extension(extension)
+		except Exception as error:
+			print(f'{extension} cannot be loaded. [{error}]')
+	client.run(TOKEN)
