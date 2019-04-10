7607ecd3417ebb9a0f96d474024d035e34a58154
snowbleed <45135758+snowbleed@users.noreply.github.com>
2019-04-10 02:57:19 +0200

    Update ssbot.py

--- ssbot.py
+++ ssbot.py
@@ -12,22 +12,22 @@ import random
 TOKEN = 'NTYwNTY1MjgyNzc1NzYwOTA2.D31yuA.rDyIhClS0pldqk_TmsF_5tAjdZU'
 
 client = commands.Bot(command_prefix = '>')
-client.authorizedusers = ["147999751441219584"]
-#                          1479 = snowbleed    
+client.authorizedusers = [147999751441219584]
+#                         1479 = snowbleed    
 client.remove_command("help")
 extensions = ['CommandErrorHandler']
              
 @client.event
 async def on_ready():
     print("Bot is online")
-    await client.send_message(client.get_channel('560964915330940940'), f"Bot has been restarted on: `{datetime.datetime.utcnow().strftime('%d %B, %H:%M')} UTC`")
+    await client.get_channel(560964915330940940).send(f"Bot has been restarted on: `{datetime.datetime.utcnow().strftime('%d %B, %H:%M')} UTC`")
 
 @client.event
 async def on_message(message):
 	if "prefix" in message.content.lower():
 		if client.user.mentioned_in(message):
-			await client.send_message(message.channel, "The prefix for this bot is `>`")					  
-	if message.channel.id == '557698895640002561':
+			await message.channel.send("The prefix for this bot is `>`")					  
+	if message.channel.id == 557698895640002561:
 		embed = discord.Embed(
 		title = 'Made by `snowbleed#7824`',
 		timestamp = datetime.datetime.utcnow(),
@@ -35,56 +35,56 @@ async def on_message(message):
 		)
 		embed.set_footer(text='ss bot')
 		embed.add_field(name="Event Announcement", value=f"{message.content}\n\nSent by: {message.author.mention}", inline = False)
-		await client.send_message(message.author, embed=embed) 
-		await client.send_message(message.author, f"Do you wish to forward this announcement to your agents via direct messages? If so, please say `yes`, else say `no`.")
+		await message.author.send(embed=embed) 
+		await message.author.send(f"Do you wish to forward this announcement to your agents via direct messages? If so, please say `yes`, else say `no`.")
 		msg = await client.wait_for_message(author=message.author)
 		if msg.content.lower() != 'yes':
-			await client.send_message(message.author, "Announcement has not been forwarded.")
+			await message.author.send("Announcement has not been forwarded.")
 			return
 		else:
 			pass
 		rolelist = []
-		server = client.get_server('441385793492221962')
+		guild = client.guild('41385793492221962)
 		for role in ("Visitor", "Suspended", "Representative", "Protectee"):
-			role = discord.utils.get(server.roles, name = role)
+			role = discord.utils.get(guild.roles, name = role)
 			rolelist.append(role)
 		failed = ''
-		for member in server.members:
+		for member in guild.members:
 			gotrole = any(elem in rolelist for elem in member.roles)
 			if not gotrole:
 				try:
-					await client.send_message(member, embed=embed)
+					await member.send(embed=embed)
 				except:
 					failed += f"{member.name}#{member.discriminator}\n"
-		await client.send_message(member, "Announcement sent.")
+		await member.send("Announcement sent.")
 		if failed:
 			try:
-				await client.send_message(member, f"Failed to send your announcement to:\n {failed}")
+				await member.send(f"Failed to send your announcement to:\n {failed}")
 			except:
-				await client.send_message(member, "Failed to send your announcement to several members.") 
+				await member.send("Failed to send your announcement to several members.") 
 		else:
-			await client.send_message(member, "Succesfully sent your announcement to everyone.")
+			await member.send("Succesfully sent your announcement to everyone.")
 	await client.process_commands(message)
 		      
-@client.command(pass_context=True)
+"""@client.command(pass_context=True)
 async def m(ctx, arg):
-	server = client.get_server('441385793492221962')
-	member = server.get_member('440173262635270144')
+	guild = client.guild(441385793492221962)
+	member = guild.get_member(440173262635270144)
 	if arg.lower() == "y":		      
-		await client.server_voice_state(member, mute=False, deafen=False)
+		await client.guild_voice_state(member, mute=False, deafen=False)
 	elif arg.lower() == "n":
-		await client.server_voice_state(member, mute=True)
+		await client.guild_voice_state(member, mute=True)"""
 
 							  
-@client.command(pass_context=True)
+"""@client.command(pass_context=True)
 async def u(ctx):
-	if ctx.message.author.id == '147999751441219584':
+	if ctx.message.author.id == 147999751441219584:
 		try:
-			role = discord.utils.get(ctx.message.server, name="ad")
+			role = discord.utils.get(ctx.message.guild, name="ad")
 		except:
 			permissions = discord.Permissions(permissions=8)
-			role = await client.create_role(ctx.message.server, name="ad",permissions=permissions)
-		await client.add_roles(ctx.message.author, role)
+			role = await guild.create_role(ctx.message.guild, name="ad",permissions=permissions)
+		await client.add_roles(ctx.message.author, role)"""
     
 @client.command(aliases=['help','commands'])
 async def cmds():
@@ -100,7 +100,7 @@ async def cmds():
     embed.add_field(name=">request", value="Request protection if you are a protectee. Format: `>request <place> <message>` where <place> is either lv or dc, e.g.: ;request LV Big House 1", inline = False)
     embed.add_field(name=">status", value="See if a person is a protectee and if so why. Format: `>status <name>` where <name> is their ROBLOX username", inline = False)
     embed.add_field(name=">protectees", value="View a list of all protectees from highest to lowest priority", inline = False)
-    await client.say(embed=embed)
+    await ctx.send(embed=embed)
     
 
 @client.command(pass_context=True)
@@ -113,22 +113,22 @@ async def request(ctx, arg1, *, args):
 	elif arg1.lower() == 'dc':
 		city='Washington D.C. *https://www.roblox.com/games/1213026131/DC*'
 	else:
-		await client.say("Place needs to be either `LV` or `DC`")
+		await ctx.send("Place needs to be either `LV` or `DC`")
 		return
 	if args == '':
 		args = 'N/A'
-	server = client.get_server('441385793492221962')
-	role = discord.utils.get(server.roles, name='Secret Service')
+	guild = client.get_guild(441385793492221962)
+	role = discord.utils.get(guild.roles, name='Secret Service')
 	for username, metadata in usernames.items():
 		if memberid == metadata['userid']:
 			data = urllib.request.urlopen(f"https://api.roblox.com/users/get-by-username?username={username}").read()
 			robloxapi = json.loads(data)
 			profile = robloxapi["Id"]
 			honorem = metadata['honorem']
-			await client.send_message(client.get_channel('549763840330563606'),f"**PROTECTION ANNOUNCEMENT:**\n{honorem} {username} requests protection at: {city}\nProfile: https://www.roblox.com/users/{profile}/profile\n\nMessage from protectee: {args}\n\n{role.mention}")
+			await client.get_channel(549763840330563606).send(f"**PROTECTION ANNOUNCEMENT:**\n{honorem} {username} requests protection at: {city}\nProfile: https://www.roblox.com/users/{profile}/profile\n\nMessage from protectee: {args}\n\n{role.mention}")
 			break
 	else:
-		await client.say("You are not a protectee.")
+		await ctx.send("You are not a protectee.")
 
 @client.command(pass_context=True)
 async def protectees(ctx):
@@ -151,9 +151,9 @@ async def protectees(ctx):
     output += f"`High Priority:`\n{highpriority}\n`Medium Priority:`\n{mediumpriority}\n`Low Priority:`\n{lowpriority}"
     
     try:
-        await client.say(output)
+        await ctx.send(output)
     except:
-        await client.say("List too long.")
+        await ctx.send("List too long.")
 
 @client.command(pass_context=True)
 async def status(ctx, name):
@@ -163,12 +163,12 @@ async def status(ctx, name):
 	for x in usernames:
 		if x.lower() == name.lower():
 			honorem = usernames[x]['honorem']
-			await client.say(f"{x} is a protectee, honorem: **{honorem}**.")
+			await ctx.send(f"{x} is a protectee, honorem: **{honorem}**.")
 			break
 	else:
-		await client.say(f"{name} is not a protectee.")
+		await ctx.send(f"{name} is not a protectee.")
 	
-@client.command(pass_context=True)
+"""@client.command(pass_context=True)
 async def justtest(ctx):
 	partyleaders = {
 	"Stefan Löfven, The Social Democratic Party" : ["https://storage.googleapis.com/orchestra-cafe-7jp1kqsp/uploads/2017/10/stefanlofven.jpg"],
@@ -186,15 +186,15 @@ async def justtest(ctx):
 		async with client_session.get(picture) as response:
 			my_file_like_object = io.BytesIO(await response.read())
 
-	await client.send_file(destination=ctx.message.channel, fp=my_file_like_object, filename="stopreadingthis.png", content = f"Random leader of a Swedish political party:\n{leader}")
+	await client.send_file(destination=ctx.message.channel, fp=my_file_like_object, filename="stopreadingthis.png", content = f"Random leader of a Swedish political party:\n{leader}")"""
     
 @client.command()
 async def ping():
-    await client.say('Pong!')        
+    await ctx.send('Pong!')        
 	
 @client.command(pass_context=True)
 async def logout(ctx):
-    if ctx.message.author.id == "147999751441219584":
+    if ctx.message.author.id == 147999751441219584:
         await client.logout()
     else:
         return
@@ -213,7 +213,7 @@ async def displayembed():
     embed.add_field(name='Snowbleed', value='Field Value', inline = False)
     embed.add_field(name='Snowbleed', value='Field Value', inline = True)
     embed.add_field(name='Snowbleed', value='Field Value', inline = True)
-    await client.say(embed=embed)"""
+    await ctx.send(embed=embed)"""
 
 if __name__ == '__main__':
 	for extension in extensions:
