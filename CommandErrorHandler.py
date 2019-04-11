3dab7b817f50d2cacb23bcfb66ed1ad91cdcca6c
snowbleed <45135758+snowbleed@users.noreply.github.com>
2019-04-12 01:18:10 +0200

    Update CommandErrorHandler.py

--- CommandErrorHandler.py
+++ CommandErrorHandler.py
@@ -24,8 +24,11 @@ class CommandErrorHandler(commands.Cog):
             return
         
         elif isinstance(error, commands.UserInputError):
-            await ctx.send('You are missing a required argument, look through the command list.')
-            return
+            return await ctx.send('You are missing a required argument, look through the command list.')
+        
+        elif isinstance(error, commands.CommandOnCooldown):
+            return await ctx.send(f'The command is currently on cooldown, please try again in {cooldown} seconds')
+               
                 
         elif isinstance(error, commands.DisabledCommand):
             return await ctx.send(f'{ctx.command} has been disabled.')
