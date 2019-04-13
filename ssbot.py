b54b420e34f301559d62443a4c6c0befda0d8a43
snowbleed <45135758+snowbleed@users.noreply.github.com>
2019-04-14 00:39:23 +0200

    Update ssbot.py

--- ssbot.py
+++ ssbot.py
@@ -210,10 +210,9 @@ async def justtest(ctx):
 @client.command()
 async def changepic(ctx):
     if ctx.message.author.id == 147999751441219584:
-        msg = await ctx.send('Changing...')
-        pic = ctx.guild.icon_url_as(format='png', size=2048)                       
+        msg = await ctx.send('Changing...')                     
         async with aiohttp.ClientSession() as client_session:
-            async with client_session.get(pic) as response:
+            async with client_session.get(ctx.guild.icon_url_as(format='png', size=2048)) as response:
                 avatar = io.BytesIO(await response.read())
         await client.edit(avatar=avatar)
         await message.edit(content='Avatar changed!')                       
